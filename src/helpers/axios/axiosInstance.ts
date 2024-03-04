86ef6c91c22a256277232ef82e146317a49de077
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2024-03-04 11:12:13 +0600

    [Axios Instance has been setup]

---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,34 @@
+import { authKey } from "@/constants/storagekey";
+import { getFromLocalStorage } from "@/utils/local-storage";
+import axios from "axios";
+
+const instance = axios.create();
+
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getFromLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+export { instance };
