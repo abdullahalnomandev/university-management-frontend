16e95845ee699c740bdaf81d668518114daf9b2c
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2024-03-09 01:15:52 +0600

    [Department Page created]

--- src/app/(withlayout)/super_admin/manage-faculty/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-faculty/create/page.tsx
@@ -1,22 +1,254 @@
+import Form from '@/components/Forms/Form';
+import FormInput from '@/components/Forms/FormInput';
+import FormSelectField from '@/components/Forms/FormSelectField';
 import UMBreadCrumb from '@/components/ui/UMBreadCrumb';
+import { bloodGroupOptions, departmentOptions, genderOptions } from '@/constants/global';
+import { Button, Col, Row } from 'antd';
 import React from 'react';
+import  UploadImage  from '@/components/ui/UploadImage';
+import FormDatePicker from '@/components/Forms/FormDatePicker';
+import FormTextArea from '@/components/Forms/FormTextArea';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const CreateFacultyPage = () => {
+
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (err: any) {
+      console.log("err", err);
+    }
+  };
+
     return (
       <div>
         <UMBreadCrumb
           items={[
             {
-              label: `super_admin`,
-              link: `/super_admin`
+              label: `super_faculty`,
+              link: `/super_faculty`
             },
             {
               label: `manage-faculty`,
-              link: `/super_admin/manage-faculty`
+              link: `/super_faculty/manage-faculty`
             }
           ]}
         />
         <h1>Create Faculty</h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div
+              style={{
+                border: "1px solid #d9d9d9",
+                borderRadius: "5px",
+                padding: "15px",
+                marginBottom: "10px"
+              }}
+            >
+              <p style={{ fontSize: "18px", marginBottom: "10px" }}>
+                faculty information
+              </p>
+              <Row gutter={{ xs: 8, sm: 8 }}>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.name.firstName"
+                    size="large"
+                    label="First Name"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.name.middleName"
+                    size="large"
+                    label="Middle Name"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.name.lastName"
+                    size="large"
+                    label="Last Name"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="password"
+                    name="password"
+                    size="large"
+                    label="Password"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormSelectField
+                    name="faculty.gender"
+                    options={genderOptions}
+                    size="large"
+                    label="Gender"
+                    placeholder="Select"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormSelectField
+                    name="faculty.managementDepartment"
+                    options={departmentOptions}
+                    size="large"
+                    label="Department"
+                    placeholder="Select"
+                  />
+                </Col>
+
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <UploadImage></UploadImage>
+                </Col>
+              </Row>
+            </div>
+
+            {/* BASIC INFO  */}
+            <div
+              style={{
+                border: "1px solid #d9d9d9",
+                borderRadius: "5px",
+                padding: "15px",
+                marginBottom: "10px"
+              }}
+            >
+              <p style={{ fontSize: "18px", marginBottom: "10px" }}>
+                Basic information
+              </p>
+              <Row gutter={{ xs: 8, sm: 8 }}>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="email"
+                    name="faculty.email"
+                    size="large"
+                    label="E-mail"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.contactNo"
+                    size="large"
+                    label="Contact Number"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.emergencyContactNo"
+                    size="large"
+                    label="Emergency Contact Number"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormDatePicker
+                    size="large"
+                    name="faculty.dateOfBirth"
+                    label="Date of birth"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormSelectField
+                    name="faculty.bloodGroup"
+                    options={bloodGroupOptions}
+                    size="large"
+                    label="Blood Group"
+                    placeholder="Select"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={8}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormInput
+                    type="text"
+                    name="faculty.designation"
+                    size="large"
+                    label="Designation"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={12}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormTextArea
+                    rows={3}
+                    name="faculty.presentAddress"
+                    label="Present Address"
+                  />
+                </Col>
+                <Col
+                  className="gutter-row"
+                  span={12}
+                  style={{ marginBottom: "10px" }}
+                >
+                  <FormTextArea
+                    rows={3}
+                    name="faculty.permanentAddress"
+                    label="Permanent Address"
+                  />
+                </Col>
+              </Row>
+            </div>
+
+            <Button type="primary" htmlType="submit">
+              Create
+            </Button>
+          </Form>
+        </div>
       </div>
     );
 };
--- src/app/(withlayout)/super_admin/management/department/create/page.tsx
+++ src/app/(withlayout)/super_admin/management/department/create/page.tsx
@@ -1,24 +1,40 @@
-import UMBreadCrumb from '@/components/ui/UMBreadCrumb';
-import React from 'react';
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { Button, Col, Row } from "antd";
 
 const CreateDepartmentPage = () => {
-    return (
-      <div>
-        <UMBreadCrumb
-          items={[
-            {
-              label: `super_admin`,
-              link: `/super_admin`
-            },
-            {
-              label: `department`,
-              link: `/super_admin/management/department`
-            }
-          ]}
-        />
-        <h1>Create User</h1>
-      </div>
-    );
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+  const base = "super_admin";
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          { label: `${base}`, link: `/${base}` },
+          { label: "department", link: `/${base}/department` }
+        ]}
+      />
+      <h1>Create Department</h1>
+      <Form submitHandler={onSubmit}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FormInput name="title" label="Title" />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          Create
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default CreateDepartmentPage;
