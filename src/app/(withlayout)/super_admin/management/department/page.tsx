18400568861be20f1b2dcf7283da09549700ce60
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2024-03-12 08:47:26 +0600

    [Reuseable Table hasBeen setup]

--- src/app/(withlayout)/super_admin/management/department/page.tsx
+++ src/app/(withlayout)/super_admin/management/department/page.tsx
@@ -2,19 +2,69 @@
 import ActionBar from "@/components/ui/ActionBar";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
 import UMTable from "@/components/ui/UMTable";
-import { getUserInfo } from "@/services/auth.service";
-import { Button } from "antd";
+import { Button, TableProps } from "antd";
 import Link from "next/link";
 
 const DepartmentPage = () => {
-  const { role } = getUserInfo() as any;
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name"
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      sorter: (a: any, b: any) => a.age - b.age
+    },
+
+    {
+      title: "Action",
+      render: (data: any) => {
+        return (
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            Delete
+          </Button>
+        );
+      }
+    }
+  ];
+
+  const tableData = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"]
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"]
+    }
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    // console.log("page",page,'pageSizeChange',pageSize);
+  };
+
+    const onTableChange = (pagination: any, filter: any, sorter: any) => {
+      const { order, field } = sorter;
+
+      console.log("pagination changed", order, field);
+    };
+
   return (
     <div>
       <UMBreadCrumb
         items={[
           {
-            label: `${role}`,
-            link: `/${role}`
+            label: `super_admin`,
+            link: `/super_admin`
           }
         ]}
       />
@@ -23,7 +73,17 @@ const DepartmentPage = () => {
           <Button type="primary">Create </Button>
         </Link>
       </ActionBar>
-      <UMTable />
+      <UMTable
+        columns={columns}
+        loading={false}
+        dataSource={tableData}
+        pageSize={5}
+        totalPages={10}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
     </div>
   );
 };
--- src/components/ui/UMTable.tsx
+++ src/components/ui/UMTable.tsx
@@ -1,79 +1,44 @@
 "use client";
-import { Button, Table, TableProps } from "antd";
+import { Table } from "antd";
 import React from "react";
 
-interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
+interface UMTableProps {
+  columns: any;
+  loading?: boolean;
+  dataSource: any;
+  pageSize?: number;
+  totalPages?: number;
+  showSizeChanger?: boolean;
+  onTableChange?: (pagination: any, filter: any, sorter: any) => void;
+  onPaginationChange?: (page: number, pageSize: number) => void;
+  showPagination?: boolean;
 }
-const UMTable = () => {
-  const columns: TableProps<DataType>["columns"] = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name"
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-      sorter: (a: any, b: any) => a.age - b.age
-    },
-
-    {
-      title: "Action",
-      render: (data) => {
-        return (
-          <Button onClick={()=>console.log(data)} type="primary" danger>
-            Delete
-          </Button>
-        );
+const UMTable: React.FC<UMTableProps> = ({
+  columns,
+  loading = false,
+  dataSource,
+  pageSize,
+  totalPages,
+  showSizeChanger,
+  onTableChange,
+  onPaginationChange,
+  showPagination = true
+}) => {
+  const paginationConfig = showPagination
+    ? {
+        pageSize,
+        total: totalPages,
+        pageSizeOptions: [5, 10, 20],
+        showSizeChanger: showSizeChanger,
+        onChange: onPaginationChange
       }
-    }
-  ];
-  const tableData: DataType[] = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"]
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["loser"]
-    }
-  ];
-
-  const onPaginationChange = (page: number, pageSize: number) => {
-    // console.log("page",page,'pageSizeChange',pageSize);
-  };
-
-  const paginationConfig = {
-    pageSize: 5,
-    total: 10,
-    pageSizeOptions: [5, 10, 20],
-    showSizeChanger: true,
-    onChange: onPaginationChange
-  };
-
-  const onTableChange = (pagination: any, filter: any, sorter: any) => {
-    const { order, field } = sorter;
-
-    console.log("pagination changed", order, field);
-  };
+    : false;
 
   return (
     <Table
-      loading={false}
       columns={columns}
-      dataSource={tableData}
+      dataSource={dataSource}
+      loading={loading}
       pagination={paginationConfig}
       onChange={onTableChange}
     />
