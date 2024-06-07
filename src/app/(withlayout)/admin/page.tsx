85d69cb9284bc4b591e6b3accaeaee85e4623b58
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2024-06-07 15:45:35 +0600

    [ Modoify ]

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AdminPage = () => {
     return (
         <div>
-            <h1>Welcome to Admin "Profile"</h1>
+            <h1>Welcome to Admin Profile </h1>
         </div>
     );
 };
